004d2a46b3c484bcda36d366511e3d8c9b566a86
biko <steven.biko@savannahinformatics.com>
2024-01-29 16:21:49 +0300

    chore: Setting up project serializers and views

--- core/urls.py
+++ core/urls.py
@@ -2,8 +2,9 @@
 URL configuration for core project.
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
+    path("blog/", include("blog.urls")),
     path('admin/', admin.site.urls),
 ]
