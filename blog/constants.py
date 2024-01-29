c551f9c7e05a0fbc2b4ec2910a7c1298a7387f33
biko <steven.biko@savannahinformatics.com>
2024-01-29 14:25:05 +0300

    chore: Category and Article data mapping

---
+++ blog/constants.py
@@ -0,0 +1,8 @@
+"""All blog constants holder."""
+
+ARTICLE_TYPES = [
+    ("UN", "Unspecified"),
+    ("TU", "Tutorial"),
+    ("RS", "Research"),
+    ("RW", "Review"),
+]
--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,33 @@
 from django.db import models
+from django.contrib.auth.models import User
+from constants import ARTICLE_TYPES
 
-# Create your models here.
+class Category(models.Model):
+    """ Represents an article category.
+    e.g., programming, Linux, testing.
+    """
+    name = models.CharField(max_length=32)
+    description = models.TextField(null=True, blank=True)
+
+    class Meta:
+        verbose_name_plural = "categories"
+
+    def __str__(self) -> str:
+        """Represent a category with a name."""
+        return f"{self.name}"
+    
+class Article(models.Model):
+    """Represents an individual article. Each article can have multiple categories. 
+    Articles have a specific type -- Tutorial, Research, Review, or Unspecified.
+    """
+    title = models.CharField(max_length=256)
+    author = models.ForeignKey(to=User, on_delete=models.CASCADE)
+    type = models.CharField(max_length=2, choices=ARTICLE_TYPES, default="UN")
+    categories = models.ManyToManyField(to=Category, blank=True, related_name="categories")
+    content = models.TextField()
+    created_datetime = models.DateTimeField(auto_now_add=True)
+    updated_datetime = models.DateTimeField(auto_now=True)
+
+    def __str__(self) -> str:
+        """Represent an article with authoe name and date."""
+        return f"{self.author}: {self.title} ({self.created_datetime.date()})"
