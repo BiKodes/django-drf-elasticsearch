f51c53797a4cf5724713fc3af3df128e1cdef977
biko <steven.biko@savannahinformatics.com>
2024-01-29 12:47:15 +0300

    chore: Initialization of the blog app

--- core/settings.py
+++ core/settings.py
@@ -25,6 +25,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # Custom app
+    'blog.apps.BlogConfig',
 ]
 
 MIDDLEWARE = [
