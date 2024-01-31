4d6411079a5d34b0a8b078c707eabb5c19481ce1
biko <steven.biko@savannahinformatics.com>
2024-01-31 13:08:05 +0300

    chore: Formalization of hystack into the project

--- core/settings.py
+++ core/settings.py
@@ -121,3 +121,14 @@ ELASTICSEARCH_DSL = {
         "ca_certs": os.getenv("PATH_TO_HTTP_CA_CRT", ""),
     }
 }
+
+# Haystack settings
+HAYSTACK_CONNECTIONS = {
+    "default": {
+        "ENGINE": "haystack.backends.elasticsearch7_backend.Elasticsearch7SearchEngine",
+        "URL": "http://127.0.0.1:9200/",
+        "INDEX_NAME": "django_flix",
+    },
+}
+# auto index to elastic search when new data is created or when data is saved in the database
+HAYSTACK_SIGNAL_PROCESSOR = "haystack.signals.RealtimeSignalProcessor"
